5fc1b7a08e62177f98f2c08452784312975eb8ce
Mlazzuri <114320499+Mlazzuri@users.noreply.github.com>
2022-10-01 22:12:52 -0300

    Add files via upload

---
+++ Ej1.js
@@ -0,0 +1,17 @@
+let nombre = "Mariano";
+let edad = 48;
+let desarrollador = "True";
+let fecha_de_nacimiento= new Date(1974, 09 ,27)
+const libro={
+    titulo:"el desarrollador",
+    autor: "Miraflores",
+    Fecha: new Date(2004),
+    url: "www.eldesarrollador.com",
+
+}
+
+
+const Datos=[nombre,edad,desarrollador,fecha_de_nacimiento,libro];
+
+console.log(Datos)
+console.log(libro)
